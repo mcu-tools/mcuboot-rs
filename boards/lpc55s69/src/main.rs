f74eece7e7ff4b66224e408a5cf6826e50ed8d38
David Brown <david.brown@linaro.org>
2023-10-25 15:25:18 -0600

    vain attempt to handle exception
    Eventually we will need to figure out how to handle the HardFault exception,
    flag that it happened and return.  It might be best to just do this in assembly.
    For now, put this code here so we can find it later, even though it doesn't
    really work.

--- boards/lpc55s69/src/main.rs
+++ boards/lpc55s69/src/main.rs
@@ -4,14 +4,16 @@
 // extern crate panic_halt;
 extern crate panic_semihosting;
 
-use cortex_m_rt::entry;
+use cortex_m_rt::{entry, exception, ExceptionFrame};
 
 use embedded_hal::digital::v2::OutputPin;
-use hal::{drivers::pins::Level};
+use hal::drivers::pins::Level;
 use lpc55_hal as hal;
 use embedded_time::rate::Extensions;
 
 use cortex_m_semihosting::{hprintln};
+
+use core::arch::asm;
 
 #[entry]
 fn main() -> ! {
@@ -38,10 +40,89 @@ fn main() -> ! {
         .into_gpio_pin(&mut iocon, &mut gpio)
         .into_output(Level::High);
 
+    // show(0x20000);
+    show(0x40000);
+
     loop {
         red.set_low().unwrap();
         hal::wait_at_least(300_000);
         red.set_high().unwrap();
         hal::wait_at_least(300_000);
     }
+}
+
+/// Marker.  This is indeed unsafe, but we only access from the safe operation, and from our fault handler.
+#[derive(Clone, Copy)]
+enum FaultState {
+    None,
+    Trying,
+    Failed,
+}
+
+static mut FAULT_STATE: FaultState = FaultState::None;
+
+/// Safely read from a given location.
+#[inline(never)]
+fn safe_read(addr: usize) -> Option<u32> {
+    let ptr = unsafe {&*(addr as *const u32)};
+    unsafe { FAULT_STATE = FaultState::Trying };
+    let value = *ptr;
+    match unsafe {FAULT_STATE} {
+        FaultState::None => panic!("This shouldn't happen"),
+        FaultState::Trying => {
+            unsafe {FAULT_STATE = FaultState::None};
+            Some(value)
+        }
+        FaultState::Failed => {
+            unsafe {FAULT_STATE = FaultState::None};
+            None
+        }
+    }
+}
+
+fn show(addr: usize) {
+    hprintln!("Code at 0x{:x} = 0x{:x?}", addr, safe_read(addr));
+}
+
+// Hard fault handler.  This will happen with the unexpected access.
+#[exception]
+unsafe fn HardFault(exn: &ExceptionFrame) -> ! {
+    match FAULT_STATE {
+        FaultState::None => (),
+        FaultState::Trying => {
+            FAULT_STATE = FaultState::Failed;
+            // Return back to the user's code.
+
+            // Return two past, so avoid retrying this particular instruction,
+            // which we "hope" was a 16-bit instruction.
+            // let pc = exn.pc + 2;
+
+            // This doesn't work, as we don't have any easy to way to get back
+            // to the unwound stack our trampoline created for us. This might be
+            // doable with a newer cortex-m-rt that would need lpc55-hal to be
+            // made to work with.
+            asm!(
+                // "/* {r0} */",
+                "/* TODO: How much to unwinde the stack? */",
+                "bx {pc}",
+                in("r0") exn.r0,
+                in("r1") exn.r1,
+                in("r2") exn.r2,
+                in("r3") exn.r3,
+                in("r12") exn.r12,
+                in("lr") exn.lr,
+                pc = in(reg) exn.pc,
+                // lr = in(reg) exn.lr,
+                );
+        }
+        FaultState::Failed => (),
+    }
+    hprintln!("\nFault!: {:#x?}", exn);
+    loop {
+    }
+}
+
+// Can this return?
+#[exception]
+fn DefaultHandler(_ex: i16) {
 }
