11b5a87b200750ec096b8c4c062e873201c281c6
David Brown <david.brown@linaro.org>
2023-10-31 16:26:43 -0600

    storage: Create initial version of storage
    `storage` attempts to be a version of embedded-storage::NorFlash and friends,
    but with a few changes:
    
      - Errors aren't dynamic.  There is just a fixed set of errors.
      - The alignments are methods not associated constants.
      - usize instead of u32.
    
    The last is just a matter of convenience.  Otherwise, this should allow for
    `&dyn Flash`, which doesn't work with the embedded-storage types.  This is
    needed for environments where the particular flash part is dynamically queried,
    or for testing, where the code may want to test a large number of
    configurations.
    
    It should also save a lot in code space when using multiple flash partitions or
    devices that have different parameters.

---
+++ storage/src/lib.rs
@@ -0,0 +1,79 @@
+//! Storage types.
+
+#![cfg_attr(not(any(feature = "std", test)), no_std)]
+
+// TODO: Do we want to use errors?
+
+pub enum Error {
+    NotAligned,
+    OutOfBounds,
+    NotWritten,
+    NotErased,
+}
+
+type Result<T> = core::result::Result<T, Error>;
+
+/// Read only interface into flash.
+pub trait ReadFlash {
+    /// What is the read size (alignment and size multiple).
+    fn read_size(&self) -> usize;
+    fn read(&mut self, offset: usize, bytes: &mut [u8]) -> Result<()>;
+    fn capacity(&self) -> usize;
+}
+
+/// Flash that can be written to.
+pub trait Flash: ReadFlash {
+    /// Write size (alignment and size multiple).
+    fn write_size(&self) -> usize;
+    /// Erase size (alignment and size multiple).
+    fn erase_size(&self) -> usize;
+
+    fn erase(&mut self, from: usize, to: usize) -> Result<()>;
+    fn write(&mut self, offset: usize, bytes: &[u8]) -> Result<()>;
+}
+
+// Utilities taken from embedded-storage for validating arguments.
+pub fn check_read<T: ReadFlash>(
+    flash: &T,
+    offset: usize,
+    length: usize,
+) -> Result<()> {
+    check_slice(flash, flash.read_size(), offset, length)
+}
+
+pub fn check_erase<T: Flash>(
+    flash: &T,
+    from: usize,
+    to: usize,
+) -> Result<()> {
+    if from > to || to > flash.capacity() {
+        return Err(Error::OutOfBounds);
+    }
+    if from % flash.erase_size() != 0 || to % flash.erase_size() != 0 {
+        return Err(Error::NotAligned);
+    }
+    Ok(())
+}
+
+pub fn check_write<T: Flash>(
+    flash: &T,
+    offset: usize,
+    length: usize,
+) -> Result<()> {
+    check_slice(flash, flash.write_size(), offset, length)
+}
+
+pub fn check_slice<T: ReadFlash>(
+    flash: &T,
+    align: usize,
+    offset: usize,
+    length: usize,
+) -> Result<()> {
+    if length > flash.capacity() || offset > flash.capacity() - length {
+        return Err(Error::OutOfBounds);
+    }
+    if offset % align != 0 || length % align != 0 {
+        return Err(Error::NotAligned);
+    }
+    Ok(())
+}
